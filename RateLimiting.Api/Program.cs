447d6297dc90ca808b68100137432f5c4e79229a
Egor Quenum <georgequenum@yahoo.com>
2024-05-17 23:35:35 +0400

    Remove boiler-plate stuff

--- RateLimiting.Api/Program.cs
+++ RateLimiting.Api/Program.cs
@@ -1,4 +1,3 @@
-
 namespace RateLimiting.Api;
 
 public class Program
@@ -7,16 +6,12 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -24,10 +19,6 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
         app.MapControllers();
 
         app.Run();
