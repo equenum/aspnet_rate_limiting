c129e04e5c99d9dfe59e8edb13578eac2fb8f074
Egor Quenum <georgequenum@yahoo.com>
2024-05-17 23:41:06 +0400

    Add controllers

---
+++ RateLimiting.Api/Controllers/MediaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimiting.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class MediaController : ControllerBase
+{
+    [HttpGet("stream")]
+    public IActionResult GetMediaStream()
+    {
+        Console.WriteLine("Media controller call received.");
+
+        var bytes = new byte[100];
+        
+        var random = new Random();
+        random.NextBytes(bytes);
+
+        return Ok(string.Join(" ", bytes));
+    }
+}
---
+++ RateLimiting.Api/Controllers/UserPreferencesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimiting.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class UserPreferencesController : ControllerBase
+{
+    [HttpGet("lang")]
+    public async Task<IActionResult> GetLanguageAsync()
+    {
+        Console.WriteLine("User preference controller call received.");
+        
+        await Task.Delay(1000);
+        return Ok("English");
+    }
+}
